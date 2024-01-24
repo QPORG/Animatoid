a09ec4e524e91575b6462c4b97a9386bf6891846
qaiik <82181231+qaiik@users.noreply.github.com>
2024-01-24 14:20:05 -0700

    AnimatoidLibrary.js

---
+++ AnimatoidLibrary.js
@@ -0,0 +1,45 @@
+class Sequencer {
+  constructor(mesh) {
+    this.o = {returnToState: true};
+    this.m = mesh;
+    this.kf = []
+    this.globalKeyFrameIndex = 0;
+  }
+
+  runAnimation() {
+    setTimeout(function() {
+      if (this.globalKeyFrameIndex > this.kf.length) return
+      this.callTweenByIndex(this.globalKeyFrameIndex)
+      this.kf[this.globalKeyFrameIndex][0].onEnd(()=> {
+        this.globalKeyFrameIndex++
+        runAnimation()
+      })
+    },this.kf[this.globalKeyFrameIndex][2])
+  }
+  setReturnToState(b) {
+    this.o.returnToState = b;
+  }
+
+  createKeyFrame(state, milliseconds, to, easingCategory, easing) {
+     const tween = new TWEEN.Tween(state, false)
+		.to(to, millisecinds) // Move to (300, 200) in 1 second.
+		.easing(TWEEN.Easing[easingCategory][easing]) // Use an easing function to make the animation smooth.
+		.onUpdate((stateUpdate) => {
+			if (stateUpdate.position) {
+        this.m.position.x = stateUpdate.position.x
+        this.m.position.y = stateUpdate.position.y
+        this.m.position.z = stateUpdate.position.z
+      }
+		})
+
+    this.kf.push([tween, to, delayAfter])
+  }
+
+  callTweenByIndex(i) {
+    this.kf[i][0].start()
+  }
+
+  getChild(n) {
+    return new Sequencer(this.m.children[n]);
+  }
+}
